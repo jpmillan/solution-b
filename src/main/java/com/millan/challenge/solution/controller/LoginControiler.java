386e38eb79818a301f6a1322c25dd951ff88896f
JP Millan <jp.millan@flybuys.com.au>
2021-01-24 22:45:12 +1100

    refactor login api to handle bad requests with http codes and add logging

--- src/main/java/com/millan/challenge/solution/controller/LoginControiler.java
+++ src/main/java/com/millan/challenge/solution/controller/LoginControiler.java
@@ -4,11 +4,17 @@ import com.millan.challenge.solution.exception.FailedToLoginException;
 import com.millan.challenge.solution.model.Authentication;
 import com.millan.challenge.solution.model.UserCredentials;
 import com.millan.challenge.solution.service.UserAuthenticationService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static org.springframework.http.ResponseEntity.badRequest;
+import static org.springframework.http.ResponseEntity.ok;
+
 @RestController
+@Slf4j
 public class LoginControiler {
 
     @Autowired
@@ -17,21 +23,30 @@ public class LoginControiler {
 
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public Authentication userLogin(@RequestBody UserCredentials userCredentials) throws FailedToLoginException {
+    public ResponseEntity<Authentication> userLogin(@RequestBody UserCredentials userCredentials) {
 
         if (userCredentials == null || (userCredentials.getUsername() == null || userCredentials.getPassword() == null)) {
-            throw new FailedToLoginException("Missing login credentials ");
+            log.error("Missing login credentials");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         }
 
-        String token = authenticationService.authenticateUser(userCredentials.getUsername(), userCredentials.getPassword());
+        String token = null;
+        try {
+            token = authenticationService.authenticateUser(userCredentials.getUsername(), userCredentials.getPassword());
+        } catch (FailedToLoginException e) {
+            log.error(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+        }
+
 
         if (token != null) {
             Authentication authenticationResponse = new Authentication();
             authenticationResponse.setUsername(userCredentials.getUsername());
             authenticationResponse.setToken(token);
-            return authenticationResponse;
+            return ok(authenticationResponse);
         }
-        throw new FailedToLoginException(String.format(" unable to authenticate user [%s] ", userCredentials.getUsername()));
+        log.error(" unable to authenticate user [%s] "+ userCredentials.getUsername());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
     }
 
 }
